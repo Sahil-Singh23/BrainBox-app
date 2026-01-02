efb7a62670be4d879ef54aa0465a17121eb1476c
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-02 15:02:58 +0530

    created routes

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,17 @@ import mongoose from "mongoose";
 
 const app = express();
 
+app.post("api/v1/signin", (req,res)=>{
+ 
+});
+app.post("api/v1/content", (req,res)=>{
+ 
+});
+app.get("api/v1/content", (req,res)=>{
+ 
+});
+app.delete("api/v1/content", (req,res)=>{
+ 
+});
 
+app.listen(2000);
